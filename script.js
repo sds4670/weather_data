85b3f30f3f5c36dfabd3edb06e6f752665f3662e
sds4670 <sibinsimon123@gmail.com>
2025-11-25 08:16:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function searchCity() {
     const city = document.getElementById("city").value;
-    const API_KEY = "599c5a3be326a03950e18d7fd2a5cb4a"  // Replace
+    const API_KEY = "f6eadda50ccc40fe51178b8749a98c93"  // Replace
 
     document.getElementById("output").innerText = "Loading...";
 
